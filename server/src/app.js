babe8829df1982790b65a854aa1b0bbd3adf639c
Antoskin <spinne92e@gmail.com>
2018-03-25 13:43:37 +0300

    created get request

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,25 @@
-console.log('server run');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import morgan from 'morgan';
+
+
+const app = express();
+app.use(morgan('combime'))
+app.use(bodyParser.json());
+app.use(cors())
+
+// first back-end app url -> http://localhost:8081/status
+app.get('/status', (req, res) => {
+	res.send({
+		message: "hola world"
+	})
+})
+
+app.listen(process.env.PORT || 8081)
